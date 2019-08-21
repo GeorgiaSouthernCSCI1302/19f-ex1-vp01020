f7089bfab13cd1d22c97ff4205590bca2d712034
vp01020 <vp01020@CEIT2553214X022.ad.georgiasouthern.edu>
2019-08-21 16:54:51 -0400

    Added last name by use input

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,10 +1,18 @@
 package assignment;
+import java.util.Scanner;
+
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello my name is Viraj!");
-		System.out.println("aa gorilla ek number no chut che");
+		
+		String LastName;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Your Last Name");
+		LastName = input.nextLine();
+		
+		System.out.println("Hello my name is Viraj " + LastName + "!");
+		
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
