357eaaf9a018b847eafc1090a92676242c088856
vp01020 <vp01020@CEIT2553214X022.ad.georgiasouthern.edu>
2019-08-21 16:34:58 -0400

    Replaced hello world message to my name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,8 +3,8 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
+		System.out.println("Hello my name is Viraj!");
+		System.out.println("aa gorilla ek number no chut che");
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
